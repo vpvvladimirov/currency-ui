d1419b25cc54a9c6e7b48bc58331494c205bdc54
Vladimir <vladiv291@gmail.com>
2023-07-06 16:30:31 +0300

    Add logout logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css';
-import alivolutLogo from './images/alivolut-logo 1.png';
-import logoutIcon from './images/logout-icon.png';
+import Header from './components/header/header';
 
 const App = () => {
   return (
-    <>
-      <header className="alivolut-header">
-        <img className="alivolut-logo" src={alivolutLogo} alt="Alivolut" />
-        <img className="logout-icon" src={logoutIcon} alt="Log Out" />
-      </header>
-      <main>
-        main
-      </main>
-    </>
+    <main>
+      <Header />
+      main
+    </main>
   );
 };
 
